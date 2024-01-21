c8f5a95e6da3b3706eac2b742739a14951bd296a
IEsquivel2000 <iesquivel200010@gmail.com>
2024-01-21 14:41:38 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,28 +1,123 @@
-alert("¡Bienvenida y bievenido a nuestro sitio web!");
+let again = true;
+// Practica de if Else de JavaScript
+/*
+    Primer Challenge
+*/
+alert('Primer Challenge');
+do{
+   try{
+       let week = prompt("Que dia de la semana es?");
+       if(week == "Sabado" | "Domingo"){
+           alert("¡Buen fin de semana!");
+       }else{
+           alert("¡Buena semana!");
+       }
+   }catch(e){
+    alert('Error');
+    again = false;
+   } 
 
-// Practica de sintaxys de JavaScript
-let nombre = "Luna";
 
-let edad = 25;
+   
+   let ask = prompt("Desea ejecutar el primer Challenge nuevamente? Y/N");
+   if(ask == "Y" | ask == "y"){
+    again = true;
+   }else if(ask =="N" | ask == "n"){
+    again = false;
+   }
+}while(again == true);
 
-let numeroDeVentas = 50;
 
-let saldoDisponible = 1000;
+/*
+    Segundo Challenge
+*/
 
-alert("¡Error! Completa todos los campos");
 
-let mensajeDeError = "¡Error! Completa todos los campos";
+alert('Segundo Challenge');
+do{
+    let num = prompt("Ingrese un numero para identificar si es positivo o negativo");
+    
+    if(num > 0){
+        alert("El numero es positivo");
+    }else if(num == 0){
+        alert("El numero es cero");
+    }
+    else{
+        alert("El numero es negativo");
+    }
+    
+     
+   let ask = prompt("Desea ejecutar el Segundo Challenge nuevamente? Y/N");
+   if(ask == "Y" | ask == "y"){
+    again = true;
+   }else if(ask =="N" | ask == "n"){
+    again = false;
+   }
+}while(again == true);
 
-alert(mensajeDeError);
 
-// Simple question for name, age Driving license 
+/*
+    Tercer Challenge
+*/
 
-nombre = prompt("Introduce el nombre del usuario");
 
-edad = prompt("Introduce la edad del usuario");
+alert('Tercer Challenge');
+do{
+    let pts = prompt("Introduzca la puntuacion obtenida");
+    
+    if(pts >= 100){
+        alert("Felicidades, has ganado!");
+    }else{
+        alert("Intenta nuevamente para ganar.");
+    }
+    
+     
+   let ask = prompt("Desea ejecutar el Tercer Challenge nuevamente? Y/N");
+   if(ask == "Y" | ask == "y"){
+    again = true;
+   }else if(ask =="N" | ask == "n"){
+    again = false;
+   }
+}while(again == true);
 
-if (edad >= 18){
 
-    alert("¡Puedes obtener tu licencia de conducir!");
+/*
+    Cuarto Challenge
+*/
 
-}
+
+alert('Cuarto Challenge');
+do{
+    let cash = prompt("Introduzca el saldo de su cuenta");
+    
+    alert(`El saldo de su cuenta es ${cash}`); 
+    
+     
+   let ask = prompt("Desea ejecutar el Cuarto Challenge nuevamente? Y/N");
+   if(ask == "Y" | ask == "y"){
+    again = true;
+   }else if(ask =="N" | ask == "n"){
+    again = false;
+   }
+}while(again == true);
+
+
+/*
+    Quinto Challenge
+*/
+
+
+alert('Quinto Challenge');
+do{
+    let name = prompt("Introduzca su nombre");
+    
+    alert(`Bienvenido Usuario ${name}`); 
+    
+     
+   let ask = prompt("Desea ejecutar el Quinto Challenge nuevamente? Y/N");
+   if(ask == "Y" | ask == "y"){
+    again = true;
+   }else if(ask =="N" | ask == "n"){
+    again = false;
+   }
+}while(again == true);
