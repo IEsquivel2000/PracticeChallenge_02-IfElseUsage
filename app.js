1329866689228c691379b690a1dcd0b39e1ae8e4
IEsquivel2000 <iesquivel200010@gmail.com>
2024-01-21 13:12:05 -0500

    Update

---
+++ app.js
@@ -0,0 +1,28 @@
+alert("¡Bienvenida y bievenido a nuestro sitio web!");
+
+// Practica de sintaxys de JavaScript
+let nombre = "Luna";
+
+let edad = 25;
+
+let numeroDeVentas = 50;
+
+let saldoDisponible = 1000;
+
+alert("¡Error! Completa todos los campos");
+
+let mensajeDeError = "¡Error! Completa todos los campos";
+
+alert(mensajeDeError);
+
+// Simple question for name, age Driving license 
+
+nombre = prompt("Introduce el nombre del usuario");
+
+edad = prompt("Introduce la edad del usuario");
+
+if (edad >= 18){
+
+    alert("¡Puedes obtener tu licencia de conducir!");
+
+}
